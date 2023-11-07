80c7daf9145058eadb759bcf8c3dbfca50543948
ptonini <pedro.tonini@hotmail.com>
2023-11-07 01:03:37 -0300

    fix: fixed connection resources names

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ resource "azurerm_virtual_network_gateway" "this" {
 
 resource "azurerm_local_network_gateway" "this" {
   for_each            = var.site2site_conns
-  name                = each.key
+  name                = "${var.name}-local-gtw-${each.key}"
   resource_group_name = azurerm_virtual_network_gateway.this.resource_group_name
   location            = azurerm_virtual_network_gateway.this.location
   gateway_address     = each.value.gateway_address
@@ -89,7 +89,7 @@ resource "azurerm_local_network_gateway" "this" {
 
 resource "azurerm_virtual_network_gateway_connection" "site2site" {
   for_each                   = var.site2site_conns
-  name                       = each.key
+  name                       = "${var.name}-site2site-${each.key}"
   resource_group_name        = azurerm_virtual_network_gateway.this.resource_group_name
   location                   = azurerm_virtual_network_gateway.this.location
   type                       = "IPsec"
@@ -112,7 +112,7 @@ resource "azurerm_virtual_network_gateway_connection" "site2site" {
 
 resource "azurerm_virtual_network_gateway_connection" "vnet2vnet" {
   for_each                        = var.vnet2vnet_conns
-  name                            = each.key
+  name                            = "${var.name}-vnet2vnet-${each.key}"
   resource_group_name             = azurerm_virtual_network_gateway.this.resource_group_name
   location                        = azurerm_virtual_network_gateway.this.location
   type                            = "Vnet2Vnet"
