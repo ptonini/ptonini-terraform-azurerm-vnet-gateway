c99b303ed29d5fbfd79588ec8aaad282d78dc3c8
ptonini <pedro.tonini@hotmail.com>
2023-11-08 14:32:52 -0300

    fix: added subnet_id output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "this" {
   value = azurerm_virtual_network_gateway.this
 }
 
+output "subnet_id" {
+  value = module.subnet.this.id
+}
+
 output "public_ip" {
   value = azurerm_public_ip.this
 }
